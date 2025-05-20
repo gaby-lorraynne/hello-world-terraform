b04ca5a439970f54df331975405c4beb4187a8cb
Gabrielly Lorraynne <gabyalmeidacom@hotmail.com>
2025-05-20 13:07:22 -0400

    feat(terraform): automatizar empacotamento das funções Lambda com archive_file

--- meu-projeto/terraform/lambda_add_item.tf
+++ meu-projeto/terraform/lambda_add_item.tf
@@ -1,3 +1,9 @@
+data "archive_file" "zip_add_item" {
+  type        = "zip"
+  source_file = "../lambda/lambda_add_item/add_item.py"
+  output_path = "${path.module}/zip/lambda_add_item.zip"
+}
+
 resource "aws_lambda_function" "adicionar_item" {
   function_name = "adicionar_item"
   runtime       = "python3.12"
@@ -5,8 +11,8 @@ resource "aws_lambda_function" "adicionar_item" {
   memory_size   = 512
   timeout       = 10
 
-  filename         = "../lambda/lambda_add_item/lambda_add_item.zip"
-  source_code_hash = filebase64sha256("../lambda/lambda_add_item/lambda_add_item.zip")
+  filename         = data.archive_file.zip_add_item.output_path
+  source_code_hash = data.archive_file.zip_add_item.output_base64sha256
 
 
   role = aws_iam_role.lambda_exec.arn
