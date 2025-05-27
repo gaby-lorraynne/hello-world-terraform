eaad03f08b1e56ce35b8ec447aac2ab579979375
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2025-05-27 14:40:58 -0300

    feat: add Lambda function and archive for listing items in main.tf

--- meu-projeto/terraform/modules/lambda/main.tf
+++ meu-projeto/terraform/modules/lambda/main.tf
@@ -21,3 +21,33 @@ resource "aws_lambda_function" "hello_terraform" {
   }
 }
 
+# ZIP da lambda de listagem
+data "archive_file" "zip_list_item" {
+  type        = "zip"
+  source_file = "../lambda/lambda_list_item/list_item.py"
+  output_path = "${path.module}/zip/lambda_list_item.zip"
+}
+
+# Função Lambda para listar itens
+resource "aws_lambda_function" "listar_item" {
+  function_name    = "listar_item"
+  handler          = "list_item.lambda_handler"
+  runtime          = "python3.12"
+  role             = var.role_arn
+  filename         = data.archive_file.zip_list_item.output_path
+  source_code_hash = data.archive_file.zip_list_item.output_base64sha256
+  memory_size      = 512
+  timeout          = 10
+  
+  environment {
+    variables = {
+      TABLE_NAME = var.table_name
+    }
+  }
+
+  tags = {
+    Name        = "listar_item"
+    Environment = "dev"
+  }
+}
+
