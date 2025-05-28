aa9dc92b0c0d3e23901b2b039b32adeeb339a5f7
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2025-05-28 11:42:41 -0300

    feat: add Lambda function for listing items with zip packaging

---
+++ meu-projeto/terraform/lambda_list_item.tf
@@ -0,0 +1,31 @@
+# ZIP da lambda de listagem
+data "archive_file" "zip_list_item" {
+  type        = "zip"
+  source_file = "../lambda/lambda_list_item/list_item.py"  # Caminho do arquivo Python
+  output_path = "${path.module}/zip/lambda_list_item.zip"
+}
+
+# Função Lambda para listar itens
+resource "aws_lambda_function" "listar_item" {
+  function_name = "listar_item"
+  runtime       = "python3.12"
+  handler       = "list_item.lambda_handler"
+  memory_size   = 512
+  timeout       = 10
+
+  filename         = data.archive_file.zip_list_item.output_path
+  source_code_hash = data.archive_file.zip_list_item.output_base64sha256
+
+  role = var.lambda_role_arn
+
+  environment {
+    variables = {
+      TABLE_NAME = var.TABLE_NAME
+    }
+  }
+
+  tags = {
+    Name        = "listar_item"
+    Environment = "dev"
+  }
+}
