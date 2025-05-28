6e1854267350ce18d3e2aef57ec539e4613f7099
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2025-05-28 11:53:20 -0300

    refactor: consolidate Lambda output variables and remove unused item listing outputs

--- meu-projeto/terraform/modules/lambda/outputs.tf
+++ meu-projeto/terraform/modules/lambda/outputs.tf
@@ -10,17 +10,14 @@ output "lambda_invoke_arn" {
   value = aws_lambda_function.hello_terraform.invoke_arn
 }
 
-output "list_item_function_name" {
-  description = "Nome da função Lambda de listar itens"
-  value       = aws_lambda_function.listar_item.function_name
+output "lambda_function_name" {
+  value = aws_lambda_function.hello_terraform.function_name
 }
 
-output "list_item_function_arn" {
-  description = "ARN da função Lambda de listar itens"
-  value       = aws_lambda_function.listar_item.arn
+output "lambda_function_arn" {
+  value = aws_lambda_function.hello_terraform.arn
 }
 
-output "list_item_invoke_arn" {
-  description = "Invoke ARN da Lambda de listar itens"
-  value       = aws_lambda_function.listar_item.invoke_arn
+output "lambda_invoke_arn" {
+  value = aws_lambda_function.hello_terraform.invoke_arn
 }
