520426ad09f2e39180f2810bd478dbd0110ab600
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2025-05-28 11:55:01 -0300

    refactor: remove unused Lambda output variables

--- meu-projeto/terraform/modules/lambda/outputs.tf
+++ meu-projeto/terraform/modules/lambda/outputs.tf
@@ -9,15 +9,3 @@ output "lambda_function_arn" {
 output "lambda_invoke_arn" {
   value = aws_lambda_function.hello_terraform.invoke_arn
 }
-
-output "lambda_function_name" {
-  value = aws_lambda_function.hello_terraform.function_name
-}
-
-output "lambda_function_arn" {
-  value = aws_lambda_function.hello_terraform.arn
-}
-
-output "lambda_invoke_arn" {
-  value = aws_lambda_function.hello_terraform.invoke_arn
-}
